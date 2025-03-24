6609ed160a9c29f108d210704b252dfed4450521
Mohamad-HuzaiFA <huzaifanazir143@gmail.com>
2025-03-25 00:49:19 +0500

    Modified login and signup, Authentication logic implemented

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = { 
+  // content: [
+  //   // "./app/**/*.{js,ts,jsx,tsx}",   // Scan all files inside app folder
+  //    //"./src/**/*.{js,ts,jsx,tsx}",   // Scan all files inside src
+  //    //"./src/app/Pages/**/*.{js,ts,jsx,tsx}", // Scan all files inside pages
+  // ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
